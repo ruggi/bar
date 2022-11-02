821c8bcaff43460a5d04b271790f570ed3e3ee7a
Federico Ruggi <federico.ruggi@shopify.com>
2022-11-02 18:17:04 +0100

    first

---
+++ utopia/storyboard.js
@@ -0,0 +1,52 @@
+import { Storyboard } from 'utopia-api'
+import { View } from 'utopia-api'
+
+export var storyboard = (
+  <Storyboard data-uid='0cd'>
+    <View
+      style={{
+        backgroundColor: '#f09',
+        position: 'absolute',
+        left: -51,
+        top: 72,
+        width: 268,
+        height: 355,
+      }}
+      data-uid='48d'
+    >
+      <View
+        style={{
+          backgroundColor: '#09f',
+          position: 'absolute',
+          left: 99,
+          top: 132,
+          width: 71,
+          height: 91,
+        }}
+        data-uid='480'
+      />
+      <View
+        style={{
+          backgroundColor: '#09f',
+          position: 'absolute',
+          left: 99,
+          top: 236.5,
+          width: 71,
+          height: 91,
+        }}
+        data-uid='168'
+      />
+      <View
+        style={{
+          backgroundColor: '#09f',
+          position: 'absolute',
+          left: 86,
+          top: 28,
+          width: 84,
+          height: 91,
+        }}
+        data-uid='750'
+      />
+    </View>
+  </Storyboard>
+)
