45040a0fdf0d79bf98c4bc5a19eaa195c464ab1a
Federico Ruggi <federico.ruggi@shopify.com>
2022-11-03 17:05:47 +0100

    update

--- utopia/storyboard.js
+++ utopia/storyboard.js
@@ -5,7 +5,7 @@ export var storyboard = (
   <Storyboard data-uid='0cd'>
     <View
       style={{
-        backgroundColor: '#f0f',
+        backgroundColor: '#fee',
         position: 'absolute',
         left: -51,
         top: 72,
