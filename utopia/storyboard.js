b18d9cd478021e802a65cd87e02896b20dc425c4
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2022-11-17 12:42:34 +0100

    Committed automatically.

--- utopia/storyboard.js
+++ utopia/storyboard.js
@@ -21,7 +21,7 @@ export var storyboard = (
       style={{
         backgroundColor: '#0f9',
         width: 279,
-        height: 382, // asd
+        height: 382, // asd asd
       }}
       data-uid='adb'
     >
