5d87f3a0e4d71e99cb6a66538220f12bd32681ed
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2022-11-18 15:38:24 +0100

    First commit

---
+++ utopia/storyboard.js
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import { Scene, Storyboard } from 'utopia-api'
+import { App } from '/src/app.js'
+import { Playground } from '/src/playground.js'
+import { View } from 'utopia-api'
+
+export var storyboard = (
+  <Storyboard data-uid='0cd'>
+    <View
+      style={{
+        backgroundColor: '#0091FFAA',
+        position: 'absolute',
+        left: 60,
+        top: 92,
+        width: 290,
+        height: 371,
+      }}
+      data-uid='299'
+    >
+      <View
+        style={{
+          backgroundColor: '#0091FFAA',
+          position: 'absolute',
+          left: 85,
+          top: 118,
+          width: 294,
+          height: 354,
+        }}
+        data-uid='aa0'
+      />
+    </View>
+  </Storyboard>
+)
