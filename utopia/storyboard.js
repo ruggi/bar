4df015a67592809b3b67554ab02fbdb034185857
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2022-11-25 12:33:22 +0100

    add img

--- utopia/storyboard.js
+++ utopia/storyboard.js
@@ -3,4 +3,19 @@ import { Scene, Storyboard } from 'utopia-api'
 import { App } from '/src/app.js'
 import { Playground } from '/src/playground.js'
 
-export var storyboard = <Storyboard data-uid='0cd' />
+export var storyboard = (
+  <Storyboard data-uid='0cd'>
+    <img
+      data-aspect-ratio-locked
+      src='./assets/Screenshot 2022-11-25 at 12.33.04 PM.png'
+      style={{
+        position: 'absolute',
+        width: 68,
+        height: 63,
+        top: 813.5,
+        left: 75,
+      }}
+      data-uid='44a'
+    />
+  </Storyboard>
+)
