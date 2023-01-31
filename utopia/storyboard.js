3a508fce746eae346d85b620d23ec8a0dd569d89
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2023-01-31 15:15:52 +0100

    ddd

--- utopia/storyboard.js
+++ utopia/storyboard.js
@@ -8,13 +8,56 @@ export var storyboard = (
   <Storyboard data-uid='0cd'>
     <View
       style={{
-        backgroundColor: '#aaaaaa33',
+        backgroundColor: '#f09',
         position: 'absolute',
         width: 202,
         height: 201,
-        backgroundColor: '#f09',
       }}
       data-uid='d01'
+    />
+    <View
+      style={{
+        backgroundColor: '#aaaaaa33',
+        position: 'absolute',
+        left: -42,
+        top: 268,
+        width: 143,
+        height: 157,
+      }}
+      data-uid='c1e'
+    />
+    <View
+      style={{
+        backgroundColor: '#aaaaaa33',
+        position: 'absolute',
+        left: 186,
+        top: 332,
+        width: 86,
+        height: 119,
+      }}
+      data-uid='605'
+    />
+    <View
+      style={{
+        backgroundColor: '#aaaaaa33',
+        position: 'absolute',
+        left: 186,
+        top: 260,
+        width: 86,
+        height: 53,
+      }}
+      data-uid='09a'
+    />
+    <View
+      style={{
+        backgroundColor: '#aaaaaa33',
+        position: 'absolute',
+        left: 113,
+        top: 343,
+        width: 55,
+        height: 128,
+      }}
+      data-uid='3f6'
     />
   </Storyboard>
 )
